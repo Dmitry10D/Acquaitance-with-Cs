151ba0d5a83b06ffeb85f1637804cb213277270c
Dmitry <dima10des@gmail.com>
2021-02-25 01:12:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acquaitance_with_Csh_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name, happy user?");
+            string username;
+            username = Console.ReadLine();
+            Console.WriteLine($"Hello, {username}! Today is {DateTime.Now}");
+           
+        }
+    }
+}
